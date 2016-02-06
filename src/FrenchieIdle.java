055cf5741534e2aae29d12efd343148b1198d171
Mike Lyons <mdl0394@gmail.com>
2016-02-06 14:45:12 -0800

    Updated tribot-lib

--- src/FrenchieIdle.java
+++ src/FrenchieIdle.java
@@ -1,5 +1,25 @@
-/**
- * Created by mike on 2/6/2016.
- */
-public class FrenchieIdle {
+import org.tribot.script.Script;
+import org.tribot.script.ScriptManifest;
+import org.tribot.script.interfaces.Painting;
+import scripts.lib.Properties;
+
+import java.awt.*;
+
+@ScriptManifest(
+        authors = { "frenchie4111" },
+        name = "Frenchie Idler",
+        category = "Tools"
+)
+public class FrenchieIdle extends Script implements Painting {
+
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void onPaint( Graphics g ) {
+        g.drawString( Properties.getProperties().getCurrentState(), 300, 300 );
+    }
 }
