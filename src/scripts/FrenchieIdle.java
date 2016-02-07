14972fa503bea3516168d6e8f26e83d055b8ae50
Mike Lyons <mdl0394@gmail.com>
2016-02-07 02:14:18 -0800

    Updated tribot-lib

--- src/scripts/FrenchieIdle.java
+++ src/scripts/FrenchieIdle.java
@@ -1,5 +1,6 @@
 package scripts;
 
+import org.tribot.api2007.util.ThreadSettings;
 import scripts.bot.chickencooker.ChickenCooker;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
@@ -9,6 +10,7 @@ import scripts.lib.TribotPrintStream;
 import scripts.lib.action.Action;
 import scripts.lib.action.LinearGroup;
 import scripts.lib.action.Login;
+import scripts.lib.action.camera.SetCameraAngle;
 import scripts.lib.condition.True;
 
 import java.awt.*;
@@ -22,12 +24,14 @@ public class FrenchieIdle extends Script implements Painting {
     Action script = new LinearGroup(
             new Action[] {
                     new Login(),
+                    new SetCameraAngle( 90, 100 ),
                     new ChickenCooker( new True() )
             }
     );
 
     @Override
     public void run() {
+        ThreadSettings.get().setClickingAPIUseDynamic( true );
         System.setOut( new TribotPrintStream( this ) );
 
         script.start();
